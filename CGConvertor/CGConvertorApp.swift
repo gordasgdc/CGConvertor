0e5dbdc4bb1f5151345c6c4d6637b6578ccb4679
Cristi Gordas <dumitrugdc@gmail.com>
2026-06-30 11:54:44 +0200

    Initial Commit

---
+++ CGConvertor/CGConvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CGConvertorApp.swift
+//  CGConvertor
+//
+//  Created by Dumitru Cristi Gordas on 30/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CGConvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CGConvertor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CGConvertor
+//
+//  Created by Dumitru Cristi Gordas on 30/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
